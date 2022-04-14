06868522799be42bfa64b0fd22c49479e9a71fb7
Laura Antunez <lauraantunez024@gmail.com>
2022-04-13 21:47:42 -0400

    finished html/css layout. Added El variables to js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var searchForm = document.querySelector("#search-form");
+var weatherDayTemp = document.querySelector("#weather-temp-day");
+var weatherDayWind = document.querySelector("weather-wind-day");
+var weatherDayHumidity = document.querySelector("weather-humidity-day");
+var weatherHumidity = document.querySelector("weather-humidity");
+var weatherHumidity = document.querySelector("weather-wind");
+var weatherHumidity = document.querySelector("weather-temp");
+var weatherCity = document.querySelector("weather-city");
+var uv = document.querySelector("uv");
+
