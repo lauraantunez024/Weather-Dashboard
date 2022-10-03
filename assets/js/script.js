819c9daa64c420feabd6fdce9cc935928b81f524
Laura Antunez <lauraantunez024@gmail.com>
2022-10-03 19:39:02 -0400

    Fixed previously hard coded html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ var weatherDayTemp = document.querySelector("#weather-temp-day");
 var weatherDayWind = document.querySelector("weather-wind-day");
 var weatherDayHumidity = document.querySelector("weather-humidity-day");
 var weatherHumidity = document.querySelector("weather-humidity");
-var weatherHumidity = document.querySelector("weather-wind");
-var weatherHumidity = document.querySelector("weather-temp");
+var weatherWind = document.querySelector("weather-wind");
+var weatherTemp = document.querySelector("weather-temp");
 var weatherCity = document.querySelector("weather-city");
-var uv = document.querySelector("uv");
+var uv = document.querySelector("city-uv");
+var weatherContainer = document.querySelector("#weather-container");
 
