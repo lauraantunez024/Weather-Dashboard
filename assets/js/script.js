e31875278ddc5b8c065e5418711d59d066b6d6e0
Laura Antunez <lauraantunez024@gmail.com>
2022-10-03 19:57:44 -0400

    added populate forecast data functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,47 @@ var weatherTemp = document.querySelector("weather-temp");
 var weatherCity = document.querySelector("weather-city");
 var uv = document.querySelector("city-uv");
 var weatherContainer = document.querySelector("#weather-container");
+var forecastContainerEl = document.querySelector("#forecast-container");
+var outerForecastContainerEl = document.querySelector("#outer-forecast-container");
+var weatherDayDateEl = document.querySelector("#weather-day-date");
+
+var baseUrl = "http://api.openweathermap.org/";
+var apiKey = "18e3e4ab32179be2cd6762b22f329d49";
+
+
+function populateForecast(data){
+    forecastContainerEl.innerHTML = "";
+    data.forEach(function (day, index){
+        if(index === 0 || index > 5) {
+            return;
+        }
+    var dt = day.dt;
+    var date = moment(dt * 1000).format("L");
+    var temp = day.temp.day;
+    var wind = day.wind_speed;
+    var humidity = day.humidity;
+    var div = document.createElement("div");
+    var offsetClass = "";
+    if (index === 1) {
+        offsetClass = "col-lg-offset-1";
+    }
+    div.classList = `card-weather-container col-sm-12 ${offsetClass} col-lg-2 text-light`;
+    div.innerHTML = `
+    <div class=card-weather bg-dark p-3> 
+    <h4>${date}</h4>
+    <img src="https://openweathermap.org/img/wn/${icon}.png />
+    <dl>
+        <dt>Temp:</dt>
+        <dd>${temp}</dd>
+        <dt>Wind:</dt>
+        <dd>${wind}</dd>
+        <dt>Humidity:</dt>
+        <dd>${humidity}</dd>
+    `;
+    forecastContainerEl.appendChild(div);
+    });
+    outerForecastContainerEl.classList.remove("hide");
+    }
+    
+
 
